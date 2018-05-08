cf6806614458daa329d0e6bfd4c7d025013ac7b0
root <root@DESKTOP-I55GPFQ.localdomain>
2018-05-09 02:04:34 +0900

    .

--- 1065.c
+++ 1065.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-int getdigit(int n,int d) {
+
+int
+getdigit(int n,int d) {
     return (n-n/(int)pow(10,d)*pow(10,d))/pow(10,d-1);
 }
-int ishan(int n) {
+
+int
+ishan(int n) {
     int t=n,c=1;
     int ps=-99,s=0;
     for(;t>=10;c++)
@@ -15,7 +19,9 @@ int ishan(int n) {
     }
     return 1;
 }
-int main() {
+
+int
+main() {
     int n,c=0;
     scanf("%d",&n);
     for(int i=1;i<=n;i++) {
