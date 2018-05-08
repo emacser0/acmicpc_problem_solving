3e8c1a574ae8469d26dd3375da6e58f3b125896c
root <root@DESKTOP-I55GPFQ.localdomain>
2018-05-09 01:48:14 +0900

    .

---
+++ 1110.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int n,cur=999,t1,t2,i;
+int main() {  
+  scanf("%d",&n);
+  for(;n!=cur;i++) {
+      if(i==0)cur=n;
+      t1=cur-cur/10*10;
+      t2=cur/10+t1;
+      cur=t1*10+t2-t2/10*10;
+  }
+  printf("%d\n",i);
+}
--- 5622.c
+++ 5622.c
@@ -1,1 +1,7 @@
-t;main(b){for(;(b=getchar())>10;)t+=b<80?(b-65)/3+3:b<84?8:b<87?9:10;printf("%d",t);}
+#include <stdio.h>
+int t,b;
+int main() {
+  for(;(b=getchar())>10;) {
+    t+=b<80?(b-65)/3+3:b<84?8:b<87?9:10;
+  }    
+  printf("%d",t);}
