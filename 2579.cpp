0062a611d9208be65f394ccc81db343ca5fd34a1
emacser <roxman98@naver.com>
2018-10-04 18:49:53 +0900

    .

--- 2579.cpp
+++ 2579.cpp
@@ -1,42 +1,17 @@
 #include <iostream>
-#include <deque>
-int n,max,score[302],dp[2250000][2];
-void go(int i, int c,int sum) {
-  if(i<2) {
-    go(i+1,c+1,sum+score[i]);
-  }
-
-}
+#define max(a,b) a>b?a:b
+int n,stairs[302],dp[302];
 int
 main() {
   std::cin >> n;
   for(int i=0;i<n;i++) {
-    std::cin >> score[i];
-    max=score[i]>max?score[i]:max;
+    std::cin >> stairs[i];
   }
-  for(int i=0;i<max*n;i++) {
-    dp[i][1]=-1;
+  dp[0]=stairs[0];
+  dp[1]=stairs[0]+stairs[1];
+  dp[2]=max(stairs[0]+stairs[2],stairs[1]+stairs[2]);
+  for(int i=3;i<n;i++) {
+    dp[i]=max(dp[i-2]+stairs[i],dp[i-3]+stairs[i-1]+stairs[i]);
   }
-  dp[score[0]][0]=1,dp[score[0]][1]=0;
-  dp[score[1]][0]=1,dp[score[1]][1]=1;
-  for(int i=1;i<n+1;i++) {
-    for(int j=1;j<max*i;j++) {
-      if(dp[j][1]!=0 and (dp[j][1]==i-1 or dp[j][1]==i-2)) {
-        if(dp[j][0]<3) {
-          dp[j+score[i]][0]=dp[j][0]+1;
-          dp[j+score[i]][1]=i;
-        }
-        if(i<n) {
-          dp[j+score[i+1]][0]=1;
-          dp[j+score[i+1]][1]=i+1;
-        }
-      }
-    }
-  }
-  for(int i=max*n;i>0;i--) {
-    if(dp[i][1]==n-1) {
-      std::cout << i << "\n";
-      break;
-    }
-  }
+  std::cout << dp[n-1] << "\n";
 }
