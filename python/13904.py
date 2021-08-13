f8c05c5176e8971a9612ae30ce41771b6f150301
emacs GNU <roxman98@naver.com>
2021-08-14 04:17:51 +0900

    .

--- python/13904.py
+++ python/13904.py
@@ -7,16 +7,14 @@ for i in range(n):
     maxd = max(maxd, d)
     l.append((d, w))
 
-l1 = list(l)
-l1.sort(key=lambda x: x[0])
-l1.sort(key=lambda x: -x[1])
-tw1 = 0
-for t in range(maxd + 1):
-    if not len(l1):
-        break
-    l1 = list(filter(lambda x: x[0] - t > 0, l1))
+schedule = [0 for i in range(1001)]
+l.sort(key=lambda x: -x[1])
 
-    tw1 += l1[0][1]
-    l1.pop(0)
 
-print(tw1)
+for i in range(n):
+    for j in range(l[i][0], 0, -1):
+        if schedule[j] == 0:
+            schedule[j] = l[i][1]
+            break
+
+print(sum(schedule))
