a0c18291e3e5d882ab6412db5a91a108dcefcf35
Emacser <roxman98@naver.com>
2021-11-10 23:37:37 +0900

    6778 Which Alien? | python

---
+++ python/6778.py
@@ -0,0 +1,9 @@
+A = int(input())
+B = int(input())
+
+if A >= 3 and B <= 4:
+    print("TroyMartian")
+if A <= 6 and B >= 2:
+    print("VladSaturnian")
+if A <= 2 and B <= 3:
+    print("GraemeMercurian")
