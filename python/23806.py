f3b174e2ad4eac2acbea8003b1c4b3dadf3406b7
Emacser <roxman98@naver.com>
2022-01-09 18:19:06 +0900

    23806 골뱅이 찍기 - ㅁ | python

---
+++ python/23806.py
@@ -0,0 +1,9 @@
+N = int(input())
+for i in range(N):
+    print("@" * 5 * N)
+for i in range(3 * N):
+    for j in range(5 * N):
+        print([" ", "@"][j // N in [0, 4]], end='')
+    print("")
+for i in range(N):
+    print("@" * 5 * N)
