5f7a97c2c15d1f5fa7885490dd897e43ffb4f2ef
ReolSt <roxman98@naver.com>
2022-03-29 20:15:50 +0900

    20232 Archivist | python

---
+++ python/20232.py
@@ -0,0 +1,3 @@
+N = int(input())
+print((["ITMO"] + ["SPbSU"] * 2 + ["ITMO"] * 2 + ["SPbSU"] + ["ITMO"] * 5 + ["PetrSU, ITMO"] + \
+      ["SPbSU"] * 2 + ["ITMO"] * 4 + ["SPbSU"] + ["ITMO"] * 4 + ["SPbSU"] + ["ITMO"])[N - 1995])
